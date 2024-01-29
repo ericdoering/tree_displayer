4fe0ad8e04c5d09542c62348a27c75795212f751
Eric Doering <ericbenjamindoering@gmail.com>
2024-01-28 22:46:47 -0500

    slideshow functionality successfully working

--- tree_displayer/src/FetchPhotos.tsx
+++ tree_displayer/src/FetchPhotos.tsx
@@ -1,40 +1,34 @@
 import { useState, useEffect } from "react";
+import { Slider } from "./Slider";
 
-export function FetchPhotos(){
+export function FetchPhotos() {
+  const [photos, setPhotos] = useState([]);
+  const [isLoading, setIsLoading] = useState(true)
 
-    const [photos, setPhotos] = useState([]);
-    const [isLoading, setIsLoading] = useState(true)
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      try {
+        const response = await fetch("../photos/photos.json");
+        const result = await response.json();
+        const photoResult = result.photos
+    
+        setPhotos(photoResult);
+        setIsLoading(false);
 
-    useEffect(() => {
-        const fetchPhotos = async() => {
-            try{
-                const response = await fetch("../photos/photos.json")
-                const result = await response.json();
-                setPhotos(result);
-                setIsLoading(false)
-            }
-            catch(error){
-                console.log(error)
-                setIsLoading(false)
-            }
-        }
-        fetchPhotos()
-        console.log("DATA", photos)
-    }, [])
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+      }
+    };
 
+    fetchPhotos();
+  }, []);
 
-    return (
-        <>
-        {isLoading ?
-            <div>
-                <h1>Loading</h1>
-            </div>
-        : <div>
-            <h1>Not Loading</h1>
-            <p></p>
-        </div>
-        }           
-
-        </>
-    )
+  return (
+    <>
+    {isLoading ? <p>Loading ... </p>
+      : <Slider photos={photos} /> 
+    }
+  </>
+  );
 }
--- tree_displayer/src/Slider.tsx
+++ tree_displayer/src/Slider.tsx
@@ -1,6 +1,35 @@
+import { useState } from "react";
 
-export function Slider(){
+type PhotoData = {
+    id: number;
+    title: string;
+    url: string;
+    description: string;
+  };
+  
+  export function Slider({ photos }: { photos: PhotoData[] }) {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const currentPhoto = photos[currentIndex];
+
+    const handlePrev = () => {
+        setCurrentIndex((prevIndex) => (prevIndex - 1 + photos.length) % photos.length);
+      };
+    
+      const handleNext = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % photos.length);
+      };
+
+
     return (
-<></>
-    )
-}
+      <>
+        <div>
+            <div key={currentPhoto.id}>
+                <h1>{currentPhoto.title}</h1>
+                <img height={300} width={600} src={currentPhoto.url} alt={currentPhoto.description} />
+            </div>
+                <button onClick={handlePrev}>Previous</button>
+                <button onClick={handleNext}>Next</button>
+        </div>
+      </>
+    );
+  }
